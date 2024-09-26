de584991609dedc62bfbb8a4a8b0501a4f953cc5
hurtaditoo <al415507@uji.es>
2024-09-26 22:00:10 +0200

    Iteration 3.3 de dos formas, con bucle y con funciones

--- index.js
+++ index.js
@@ -30,3 +30,37 @@ for (let i = hacker2.length - 1; i >= 0; i--) {
 }
 
 console.log(reverseName)
+
+let counter = 0; 
+let isFound = false;
+while (hacker1[counter] != undefined && hacker2[counter] != undefined && !isFound) {
+    const char1 = hacker1[counter].toLowerCase();
+    const char2 = hacker2[counter].toLowerCase();
+
+    if (char1 > char2) {
+        console.log(`The driver's name goes first.`);
+        isFound = true;
+    } 
+    else if (char2 > char1) {
+        console.log(`Yo, the navigator goes first, definitely.`);
+        isFound = true;
+    } 
+    else {
+        counter++;
+    }
+}
+
+if (!isFound) {
+    if (hacker1.length > hacker2.length) {
+        console.log(`The driver goes first.`);
+    } 
+    else if (hacker2.length > hacker1.length) {
+        console.log(`Yo, the navigator goes first, definitely.`);
+    } 
+    else {
+        console.log(`What?! You both have the same name?`);
+    }
+}
+
+let number = hacker1.localeCompare(hacker2);
+console.log(number);
